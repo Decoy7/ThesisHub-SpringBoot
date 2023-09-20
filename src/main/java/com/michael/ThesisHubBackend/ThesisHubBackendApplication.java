9c5bbf1dfa12af67d9b5c54f0dd76e94d7237f4e
Michael Fotiadis <49075426+Decoy7@users.noreply.github.com>
2023-09-20 13:28:05 +0300

    Initial commit

---
+++ src/main/java/com/michael/ThesisHubBackend/ThesisHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.michael.ThesisHubBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesisHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThesisHubBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michael/ThesisHubBackend/ThesisHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michael.ThesisHubBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesisHubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
